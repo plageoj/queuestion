696dad7bd476102769daa2e7c027a248790a16b5
Masayuki Sugahara <equaaqua@hotmail.com>
2018-02-26 21:59:50 +0900

    Added publish and subscribe definitions

--- server/main.js
+++ server/main.js
@@ -1,5 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.startup(() => {});
+
+Meteor.publish('Questions', (type, query, limit) => {
+    if (!limit) limit = 30;
+    switch (type) {
+        case 'public':
+            return Questions.find({ isPublic: true }, { limit: limit });
+        case 'specific':
+            return Questions.find({ _id: query }, { limit: limit });
+        case 'users':
+            return Questions.find({ userId: query }, { limit: limit });
+    }
 });
--- shared/Routing.js
+++ shared/Routing.js
@@ -1,12 +1,15 @@
 Router.route('/', function() {
+    this.subscribe('Questions', 'public');
     this.render('Home');
 });
 Router.route('/user/:userid', function() {
+    this.subscribe('Questions', 'users', this.params.userid);
     this.render('User');
 });
 Router.route('/user/:userid/q', function() {
     this.render('Enqueue');
 });
 Router.route('/q/:qid', function() {
+    this.subscribe('Questions', 'specific', this.params.qid);
     this.render('Question');
 });
